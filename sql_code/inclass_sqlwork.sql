c7df968101b35853882655094ba016e616801a72
HoldenEagle <166951588+HoldenEagle@users.noreply.github.com>
2026-03-16 19:14:38 -0400

    make authors2 and sql code

--- sql_code/inclass_sqlwork.sql
+++ sql_code/inclass_sqlwork.sql
@@ -1,41 +1,3 @@
-CREATE TABLE IF NOT EXISTS grants(
-  id integer primary key autoincrement,
-  application_id varchar(20) not null,
-  start_at date,
-  grant_type varchar(10),
-  total_cost integer
-);
-
-CREATE TABLE authors (
-    author_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    pmid INTEGER,
-    name TEXT,
-    initials TEXT,
-    affiliation TEXT
-);
-
-
-CREATE TABLE grantees (
-    application_id INTEGER,
-    pi_name TEXT
-);
-
-#We got about 15,800 matches between authors and grantees
-SELECT COUNT(*)
-FROM authors a
-JOIN grantees g
-ON a.name = g.pi_name;
-
-CREATE TABLE author_grantee_bridge AS
-SELECT
-    a.PMID,
-    g.application_id
-FROM authors a
-JOIN grantees g
-ON a.name = g.pi_name;
-
-
-
 /*
 Select 100 rows from a table, including all columns.
 Select only the forenmae or title column from a table.
@@ -48,11 +10,14 @@ Find all columns in which the forename is 'John'.
 9. Join together tables on forename, surname or on organization.
 10. Only find identical matches
 11. Find all possible matches for a single surname
-If you don't have names in your db, you can either put them in or extract the first word of each title and treat it as a "name" SELECT substr(sentence, 1, instr(sentence, ' ') - 1) AS first_word FROM your_table;
+12. If you don't have names in your db, you can either put them in or extract the first word of each title and treat it as a "name" SELECT substr(sentence, 1, instr(sentence, ' ') - 1) AS first_word FROM your_table;
 How many unique surnames do you have? If not surnames, affiliations?
-Find everyone who works at the same organization as a person named John (requires subqueries).
-Find all people who work at an organization that appears more than once in the table (subqueries and cleverness)
-If you complete this, what are the pandas equivalents for each?
+
+13. Find everyone who works at the same organization as a person named John (requires subqueries).
+
+14. Find all people who work at an organization that appears more than once in the table (subqueries and cleverness)
+
+15. If you complete this, what are the pandas equivalents for each?
 */
 
 
@@ -120,8 +85,39 @@ authors auth
 ON art.Affiliation = auth.affiliation;
 
 /* Question 11 */
+WITH newtab as
+  (SELECT * FROM authors2 LIMIT 1)
 SELECT * FROM 
 articles art
-inner JOIN
-SELECT * FROM authors2 LIMIT 1 AS auth2
-ON art.LastName = auth.LastName;
+JOIN
+newtab
+ON art.LastName = newtab.LastName;
+
+
+SELECT * FROM articles
+WHERE LastName = 'Al Shobaili';
+
+/*Question 12 */
+SELECT COUNT(distinct(LastName))
+FROM articles;
+
+
+/* Question 13 */
+WITH john_work AS
+  (SELECT * FROM authors2 WHERE FirstName = 'John')
+SELECT * FROM authors2
+INNER JOIN
+john_work
+WHERE authors2.affiliation = john_work.affiliation
+  AND authors2.FirstName <> 'John';
+
+
+/*Question 14 */
+WITH affiliation_count AS 
+(
+SELECT affiliation, COUNT(author_id) AS num_employees from authors2
+GROUP BY affiliation
+  HAVING affiliation IS NOT NULL
+)
+SELECT * FROM affiliation_count
+WHERE num_employees > 1;
