82074a5eb2cbbdb98cdab13a7d0836c3a54d4878
HoldenEagle <166951588+HoldenEagle@users.noreply.github.com>
2026-04-20 19:42:59 -0400

    add simulation file

---
+++ scripts/sir.py
@@ -0,0 +1,10 @@
+#lets generate the simulation of SIR model (susceptible, infected , and recovered)
+#parameters: beta (contagious rate) , gamma (recovery time)
+
+
+#lets initialize gamma and beta
+gamma = 0.1
+beta = 0.4
+
+#implement these functions St+1−St=−βItStN,It+1−It=βItStN−γIt,Rt+1−Rt=γIt
+
